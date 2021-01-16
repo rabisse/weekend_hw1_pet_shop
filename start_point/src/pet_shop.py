152a81e10ada8395c0531d8615db6bf3a59be7fd
rabisse <rabisse@gmail.com>
2021-01-16 21:19:28 +0000

    on to the optionals

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -54,3 +54,11 @@ def remove_customer_cash(customer, less_cash):
     customer["cash"] -= less_cash
 
 
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+
+
