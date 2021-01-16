405198b7e282f72786fef43ac09aeb4dd8e9e85e
rabisse <rabisse@gmail.com>
2021-01-16 21:07:43 +0000

    removed customer cash

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -7,13 +7,13 @@ def get_total_cash(shop):
     return shop["admin"]["total_cash"]
 
 def add_or_remove_cash(shop, change_in_cash):
-    shop["admin"]["total_cash"] = shop["admin"]["total_cash"] + change_in_cash
+    shop["admin"]["total_cash"] += change_in_cash
 
 def get_pets_sold(shop):
     return shop["admin"]["pets_sold"]
 
 def increase_pets_sold(shop, num_pets_sold):
-    shop["admin"]["pets_sold"] = shop["admin"]["pets_sold"] + num_pets_sold
+    shop["admin"]["pets_sold"] += num_pets_sold
 
 def get_stock_count(shop):
     return len(shop["pets"])
@@ -32,12 +32,25 @@ def find_pet_by_name(shop, name):
     for pet in shop["pets"]:
         if name == pet["name"]:
             found_pet = pet
-            return pet
+    return found_pet
 
+############################
 def remove_pet_by_name(shop, name):
-    for pet in (shop["pets"]):
+    pets = shop["pets"]
+    for index, pet in enumerate(pets):
         if name == pet["name"]:
-            shop["pets"].clear() # is this clearing all the entries or just one?
+            del pets[index]
 
-            
+
+def add_pet_to_stock(shop, new_pet):
+    shop["pets"].append(new_pet)
+
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+
+def remove_customer_cash(customer, less_cash):
+    customer["cash"] -= less_cash
+
 
